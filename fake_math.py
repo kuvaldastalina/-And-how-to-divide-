60a6cb8a00146e693c9a00ec50c54c17f980383b
kuvaldastalina <m.kopytenko@yandex.ru>
2024-08-24 21:08:33 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        return 'ОШИБКА!!!!'
+    else:
+        result = first/second
+        return result
+
+
+
---
+++ module4.1.py
@@ -0,0 +1,13 @@
+from true_math import divide as div
+
+from fake_math import divide as vid
+
+
+result1 = div(69, 3)
+result2 = div(3, 0)
+result3 = vid(49, 7)
+result4 = vid(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
